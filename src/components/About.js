1144f4950b6f7675064ee56b4bd5ed89a12d8861
shraddha-123P <shraddha.pohankar93@gmail.com>
2026-03-24 16:26:35 +0530

    update

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,10 @@
+import User from "./User";
+
 const About = ()=> {
     return (
         <div>
         <h1>"About Us"</h1>
+        <User/>
         </div>
 
     );
---
+++ src/components/User.js
@@ -0,0 +1,9 @@
+const User = () => {
+    return (
+    <div className="user-card">
+    <h2>Name: Shraddha</h2>
+    <h3>Location: Pune</h3>
+    </div>
+    );
+};
+export default User;
