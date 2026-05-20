2962646aa730e08fd8f868f12411c52250c47918
shraddha-123P <shraddha.pohankar93@gmail.com>
2026-05-20 21:14:41 +0530

    update

--- src/components/__tests__/sum.test.js
+++ src/components/__tests__/sum.test.js
@@ -3,5 +3,5 @@ import { sum } from "../sum";
 test ("sum function should calc sum of two nos.", ()=>{
     const result = sum(3,4);
 // Assertion
-expect(result).toBe(7);
+expect(result).toBe(7) ;
 });
